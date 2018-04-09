1465f2833a63b8119e980cbdaf2e6cec89fad866
micsmi2959 <mic.smiechowicz@gmail.com>
2018-04-09 13:29:26 +0200

    initializer engine

--- lib/przelewy24_payment/engine.rb
+++ lib/przelewy24_payment/engine.rb
@@ -1,4 +1,13 @@
 module Przelewy24Payment
   class Engine < Rails::Engine
+
+    engine_name 'spree_przelewy24payement'
+
+    config.autoload_paths += %W(#{config.root}/lib)
+
+    initializer "spree.gateway.payment_methods", :after => "spree.register.payment_methods" do |app|
+      app.config.spree.payment_methods << Spree::PaymentMethod::Przelewy24Payement
+    end
+
   end
 end
